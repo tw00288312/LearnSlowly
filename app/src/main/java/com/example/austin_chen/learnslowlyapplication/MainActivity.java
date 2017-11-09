15921b3597f35b4215fb964a1e45ff0a2050a516
austin.kw_chen <Austin.KW_Chen@htc.com>
2017-11-09 18:12:09 +0800

    first commit

---
+++ app/src/main/java/com/example/austin_chen/learnslowlyapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.austin_chen.learnslowlyapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void displayNextString(View view)
+    {
+        TextView textView = (TextView)findViewById(R.id.articleTextView);
+        textView.setText(R.string.article_2);
+    }
+}
